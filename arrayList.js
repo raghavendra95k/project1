4e475ff51292e42c74e3c2327d287b133870e07f
raghavendra95k <raghav.achar.95@gmail.com>
2019-12-22 01:49:21 -0800

    Initial commit

--- arrayList.js
+++ arrayList.js
@@ -1,4 +1,25 @@
-var arrayStr = {"lists" :  ["Home","back","close","continue"]};
+var arrayStr;
+function myFunctiononload()
+{
+    var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       arrayStr=JSON.parse(xhttp.responseText);
+
+    var addList = "";
+    var listLength = arrayStr.lists.length;
+   for( var i=0; i<listLength; i++)
+    {
+addList += "<li>" + arrayStr.lists[i] +"</li>";
+    }
+    document.getElementById("showList").innerHTML = addList;
+}
+};
+xhttp.open("GET", "arraListJson.json", true);
+xhttp.send();    
+}
+
 function myFunction()
 {
     var addList = "";
@@ -12,18 +33,19 @@ function myFunction()
     {
 addList += "<li>" + arrayStr.lists[i] +"</li>";
     }
-    document.getElementById("showList").innerHTML = addList;    
+    document.getElementById("showList").innerHTML = addList;
+    
 }
 
 function delFunction()
 {
-    var delList = document.getElementById("textBox").value;
+        var delList = document.getElementById("textBox").value;
     var addList = "";
     
      var delVal = arrayStr.lists.indexOf(delList);
      if(delVal>=0)
      {
-     arrayStr.lists.splice(delVal,1); 
+        arrayStr.lists.splice(delVal,1); 
      }
     var listLength = arrayStr.lists.length;
    for( var i=0; i<listLength; i++)
@@ -31,6 +53,8 @@ function delFunction()
 
 addList += "<li>" + arrayStr.lists[i] +"</li>";
   }
-    document.getElementById("showList").innerHTML = addList;    
+    document.getElementById("showList").innerHTML = addList; 
+    
+   
 }
 
